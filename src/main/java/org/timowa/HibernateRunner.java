1dae4c6b50f389eec385b7db88abc59352bed8c4
Timowa <sd3059272@gmail.com>
2024-12-20 17:33:52 +0500

    Locks

--- src/main/java/org/timowa/HibernateRunner.java
+++ src/main/java/org/timowa/HibernateRunner.java
@@ -4,18 +4,25 @@ import lombok.extern.slf4j.Slf4j;
 import org.timowa.entity.*;
 import org.timowa.util.HibernateUtil;
 
+import javax.persistence.LockModeType;
+
 @Slf4j
 public class HibernateRunner {
     public static void main(String[] args) {
         try (var sessionFactory = HibernateUtil.buildSessionFactory();
-             var session = sessionFactory.openSession()) {
+             var session = sessionFactory.openSession();
+             var session1 = sessionFactory.openSession()) {
             session.beginTransaction();
+            session1.beginTransaction();
 
-            session.enableFetchProfile("withCompany");
-            var user = session.get(User.class, 2L);
-            System.out.println(user.getCompany().getName());
+            var payment = session.find(Payment.class, 3L, LockModeType.OPTIMISTIC);
+            payment.setAmount(payment.getAmount() + 10);
+
+            var theSamePayment = session.find(Payment.class, 3L, LockModeType.OPTIMISTIC);
+            theSamePayment.setAmount(theSamePayment.getAmount() + 20);
 
             session.getTransaction().commit();
+            session1.getTransaction().commit();
         }
     }
 }
--- src/main/java/org/timowa/entity/Payment.java
+++ src/main/java/org/timowa/entity/Payment.java
@@ -1,6 +1,8 @@
 package org.timowa.entity;
 
 import lombok.*;
+import org.hibernate.annotations.OptimisticLockType;
+import org.hibernate.annotations.OptimisticLocking;
 
 import javax.persistence.*;
 
@@ -9,13 +11,19 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Builder
 @Entity
+@OptimisticLocking(type = OptimisticLockType.VERSION)
 public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private Integer amount;
 
-    @ManyToOne
+    @Version
+    private Long version;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
     @JoinColumn(name = "receiver_id")
     private User receiver;
 }
